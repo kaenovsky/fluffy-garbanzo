ef5577040000c7c1f97e65faf5901500e450575f
maca-quuack <macarena.villa@quuack.com>
2021-02-02 21:58:08 -0300

    agrego link a seccion flyers

--- src/components/Flyers.js
+++ src/components/Flyers.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Carousel} from 'antd'
+import { Carousel } from 'antd'
 
 import image1 from '../assets/images/flyers/viernes-30-10.jpg'
 import image2 from '../assets/images/flyers/viernes-28-11.jpg'
@@ -8,26 +8,30 @@ import image4 from '../assets/images/flyers/viernes-06-11.jpg'
 
 
 function Flyers() {
-    const slideStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-      };
+  const slideStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  };
 
-    const imgStyle = {
-        width: '100%',
-      };
+  const imgStyle = {
+    width: '100%',
+  };
 
 
 
-    return (
-        <Carousel autoplay>
-            <div style={slideStyle}> <img src={image1} style={imgStyle} /> </div>
-            <div style={slideStyle}> <img src={image2} style={imgStyle} /> </div>
-            <div style={slideStyle}> <img src={image3} style={imgStyle} /> </div>
-            <div style={slideStyle}> <img src={image4} style={imgStyle} /> </div>
-        </Carousel>
-    );
+  return (
+    <section id="flyers">
+
+      <h2>Flyers</h2>
+      <Carousel autoplay>
+        <div style={slideStyle}> <img src={image1} style={imgStyle} /> </div>
+        <div style={slideStyle}> <img src={image2} style={imgStyle} /> </div>
+        <div style={slideStyle}> <img src={image3} style={imgStyle} /> </div>
+        <div style={slideStyle}> <img src={image4} style={imgStyle} /> </div>
+      </Carousel>
+    </section>
+  );
 }
 
 export default Flyers;
